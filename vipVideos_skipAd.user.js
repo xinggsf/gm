68d28814fa73413a91dbd32514c080073e741d0d
xinggsf <xinggsf@gmail.com>
2016-06-11 17:12:15 +0800

    Signed-off-by: xinggsf <xinggsf@gmail.com>

--- vipVideos_skipAd.user.js
+++ vipVideos_skipAd.user.js
@@ -10,15 +10,19 @@
 // @exclude        http://*.dj92cc.com/*
 //全面支持音悦台HTML5播放，详见 https://greasyfork.org/scripts/14593
 // @exclude        http://*.yinyuetai.com/*
-// @version        2016.6.1
+// @version        2016.6.11
 // @encoding       utf-8
 // @grant          unsafeWindow
+// @grant          GM_setValue
+// @grant          GM_getValue
+// @grant          GM_setClipboard
 // ==/UserScript==
 
 -function(doc, bd) {
 "use strict";
 let isEmbed, swfAddr,
-playerAddrs = [],//已处理flash
+regYk = /VideoIDS=(\w+)/,
+playerAddrs = ['http://minggo.coding.io/swf/'],//已处理flash
 swfWhiteList = [
 	'.pdim.gs/static/',//熊猫直播
 	'http://v.6.cn/apple/player/',
@@ -27,33 +31,26 @@ swfWhiteList = [
 ],
 PLAYER_URL = [
 	{
-		urls: [
-			/^http:\/\/static\.youku\.com\/v.*?(?:play|load)er/,
-			/^http:\/\/player\.youku\.com\/player\.php\/sid\/(\w+)/,
-			/^http:\/\/cdn\.aixifan\.com\/player\/cooperation\/AcFunXYouku\.swf/,
-		],
-		varsMatch: /VideoIDS=(\w+)/,
-		format: youkuFormat,
-		isProc: function(p, fv, src) {
-			playerAddrs.push('http://minggo.coding.io/swf/player.swf');
-			switch(location.hostname) {
-			case 'v.youku.com':
-				setTimeout(scrollTo(0, 99), 9);
-				unsafeWindow._ssPlayer = p.outerHTML.replace('direct','gpu');
-				unsafeWindow.document.querySelector("div#ab_pip").outerHTML =
-				'<a style="font-size:20px;" onclick="document.querySelector(\'#movie_player\').outerHTML=_ssPlayer, delete _ssPlayer, this.parentNode.removeChild(this);">换原播放器</a>';
-				return !0;
-			case 'www.acfun.tv':
-			case 'acfun.tudou.com':
-				fv = fv.match(/vid=(\w+)/);
-				fv && setPlayer(p, youkuFormat(fv[1]));
-				return !1;
-			default:
-				fv = src.match(this.urls[1]) || src.match(this.varsMatch) ||
-					fv.match(this.varsMatch);
-				fv && ykOutsitePlayer(fv[1], p);
-				return !1;
-			}
+		urls: [/^http:\/\/static\.youku\.com\/v.*?(?:play|load)er/],
+		run: function(p, v) {
+			setTimeout(scrollTo(0, 99), 9);
+			unsafeWindow._ssPlayer = p.outerHTML.replace('direct','gpu');
+			unsafeWindow.document.querySelector("div#ab_pip").outerHTML =
+			'<a style="font-size:20px;" onclick="document.querySelector(\'#movie_player\').outerHTML=_ssPlayer, delete _ssPlayer, this.parentNode.removeChild(this);">换原播放器</a>';
+			v = v.match(regYk);
+			v && setPlayer(p, youkuFormat(v[1]));
+		}
+	},{//acfun.youku
+		urls: [/^http:\/\/cdn\.aixifan\.com\/player\/cooperation\/AcFunXYouku\.swf/],
+		run: function(p, v) {
+			v = v.match(/vid=(\w+)/);
+			v && setPlayer(p, youkuFormat(v[1]));
+		}
+	},{//youku outsite
+		urls: [/^http:\/\/player\.youku\.com\/player\.php\/sid\/(\w+)/],
+		run: function(p, v) {
+			v = swfAddr.match(this.urls[0]) || swfAddr.match(regYk) || v.match(regYk);
+			v && ykOutsitePlayer(v[1], p);
 		}
 	},{//iqiyi out
 		urls: [
@@ -61,38 +58,41 @@ PLAYER_URL = [
 			//^http:\/\/dispatcher\.video\.qiyi\.com\/disp\/shareplayer\.swf/,
 			//^http:\/\/cdn\.aixifan\.com\/player\/cooperation\/AcFunXQiyi\.swf/,
 		],
-		run: (p, src) => qyOutsiteFormat(p)
-	},{
-		urls: [
-			/^http:\/\/www\.iqiyi\.com\/common\/flashplayer/,
-		],
-		run: (p, src) => {
-			var v = getFlashvars(p).replace(/&(?:cid|tipdataurl|\w+?Time|cpn\w|\w*?loader|adurl|yhls|exclusive|webEventID|videoIsFromQidan)=[^&]*/g,'') + '&cid=qc_100001_300089';
-			setPlayer(p, iqiyiFormat(src,v));
-		}
-	},
+		run: qyOutsiteFormat
+	},{//iqiyi
+		urls: [/^http:\/\/www\.iqiyi\.com\/common\/flashplayer/],
+		run: iqiyiFormat
+	},/* {//qq video
+		urls: [/^http:\/\/imgcache\.qq\.com\/tencentvideo_v1\/player\/TencentPlayer\.swf/],
+		run: doQQPlayer
+	}, */
 ];
 
 function youkuFormat(vid) {
-//下载https://raw.githubusercontent.com/xinggsf/gm/master/yk.swf到本地，可替换
 	return `<embed id="movie_player" wmode="gpu" width="100%" height="100%" src="http://minggo.coding.io/swf/player.swf" allowfullscreen="true" allowscriptaccess="always" type="application/x-shockwave-flash" flashvars="isShowRelatedVideo=true&showAd=0&show_ce=0&showsearch=0&VideoIDS=${vid}&isAutoPlay=true">`;
 }
 function ykOutsitePlayer(vid, p) {
 	setPlayer(p, `<embed id="${p.id}" wmode="gpu" allowfullscreen="true" src="http://minggo.coding.io/swf/player.swf" allowscriptaccess="always" type="application/x-shockwave-flash" width="${p.width}" height="${p.height}" flashvars="isShowRelatedVideo=false&showAd=0&show_ce=0&showsearch=0&VideoIDS=${vid}">`);
 }
-function qyOutsiteFormat(p) {
-	let v = getFlashvars(p),
-	//css = p.style.cssText,
-	tvid = v.match(/\btvId=(\w+)/i)[1],
+function qyOutsiteFormat(p, v) {
+	let tvid = v.match(/\btvId=(\w+)/i)[1],
 	definitionID = v.match(/\b(?:definitionID|sourceId|vid)=(\w+)/)[1],
 	//http://dispatcher.video.qiyi.com/disp/shareplayer.swf http://opengg.guodafanli.com/adkiller/
 	s = `<embed width="100%" height="100%" allowscriptaccess="always" wmode="gpu" allowfullscreen="true" type="application/x-shockwave-flash" id="${p.id}" src="http://minggo.coding.io/swf/iqiyi_out.swf" flashvars="vid=${definitionID}&tvid=${tvid}&autoPlay=1&showSearch=0&showSearchBox=0&autoHideControl=1&cid=qc_100001_300089&showDock=0">`;
-	playerAddrs.push('http://minggo.coding.io/swf/iqiyi_out.swf');
 	setPlayer(p, s);
 }
-function iqiyiFormat(src, fvar) {
-	return `<embed play="true" allowfullscreen="true" wmode="gpu" type="application/x-shockwave-flash" width="100%" height="100%" id="flash" allowscriptaccess="always" src="${src}" flashvars="${fvar}">`;
+function iqiyiFormat(p, v) {
+	let s = v.replace(/&(?:cid|tipdataurl|\w+?Time|cpn\w|\w*?loader|adurl|exclusive)=[^&]*/g, '') + '&cid=qc_100001_300089';
+	s = `<embed play="true" allowfullscreen="true" wmode="gpu" type="application/x-shockwave-flash" width="100%" height="100%" id="flash" allowscriptaccess="always" src="${swfAddr}" flashvars="${s}">`;
+	setPlayer(p, s);
 }
+/* function doQQPlayer(p, v) {
+	let vid = v.match(/\bvid=(\w+)/)[1],
+	cid = v.match(/\bcid=(\w+)/)[1],
+	s = `<embed id="player" wmode="gpu" width="100%" height="100%" allowscriptaccess="always" allowfullscreen="true" type="application/x-shockwave-flash" src="http://cache.tv.qq.com/QQPlayer.swf" flashvars="vid=${vid}&autoplay=1&list=2&adplay=0&cid=${cid}&showcfg=1&title=版权来源：腾讯视频&rightpanel=0&share=0&popup=0&showend=0">`;
+	setPlayer(p, s);
+	playerAddrs.push('http://cache.tv.qq.com/QQPlayer.swf');
+} */
 function openFlashGPU(p) {
 	isEmbed ? p.setAttribute('wmode', 'gpu') : setObjectVal(p, 'wmode', 'gpu');
 	//p.parentNode.replaceChild(p.cloneNode(true), p);
@@ -103,7 +103,7 @@ function isPlayer(p) {
 	if (!p.width) return !1;//p.parentNode.removeChild(p);
 	if (p.width.endsWith('%')) return !0;
 	if (parseInt(p.width) < 233 || parseInt(p.height) < 53) return !1;
-	return isEmbed ? p.matches('[allowFullScreen=true]') :
+	return isEmbed ? p.matches('[allowFullScreen]') :
 		/"allowfullscreen"/i.test(p.innerHTML);
 	//[].some.call(p.children, t => t.name.toLowerCase() === 'allowfullscreen')
 }
@@ -141,18 +141,9 @@ function getFlashvars(p) {
 	return p.children[s].value;
 }
 function doPlayer(e) {
-	let t, v;
-	for (t of PLAYER_URL) {
+	for (let t of PLAYER_URL) {
 		if (t.urls.some(reg => reg.test(swfAddr))) {
-			if (t.run) {
-				t.run(e, swfAddr);
-				return;
-			}
-			v = getFlashvars(e);
-			if (!t.isProc || t.isProc(e, v, swfAddr)) {
-				v = v.match(t.varsMatch);
-				v && setPlayer(e, t.format(v[1]));
-			}
+			if (t.run) t.run(e, getFlashvars(e));
 			return;
 		}
 	}
@@ -167,7 +158,7 @@ new MutationObserver(function() {
 		if (isEmbed && k.parentNode.matches('object'))
 			continue;
 		swfAddr = isEmbed ? k.src : k.data || k.children.movie.value;
-		if (!/\.swf(?:$|\?)/.test(swfAddr) || playerAddrs.indexOf(swfAddr) !== -1)
+		if (!/\.swf(?:$|\?)/.test(swfAddr) || playerAddrs.some(s => swfAddr.startsWith(s)))
 			continue;
 		playerAddrs.push(swfAddr);
 		if (isPlayer(k)) {
@@ -175,8 +166,13 @@ new MutationObserver(function() {
 			doPlayer(k);
 		}
 	}
-}).observe(bd, {childList: true});
+}).observe(bd, {childList: true, subtree: true});
 let div = doc.createElement('div');
 bd.appendChild(div);
 bd.removeChild(div);
+if (GM_getValue('unread', !0)) {
+	GM_setValue('unread', !1);
+	if (confirm('作者已经迭代了几十个版本，为更好的支持后续开发，你愿意捐助吗？\n（支付宝帐号xinggsf@21cn.com已复制到剪贴板，捐助后保留转帐信息可能有惊喜）'))
+		GM_setClipboard('xinggsf@21cn.com');
+}
 }(document, document.body);
