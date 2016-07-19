b78fb040489e90d9ec86006880792d57d10706df
xinggsf <xinggsf@gmail.com>
2016-07-19 08:53:24 +0800

    Signed-off-by: xinggsf <xinggsf@gmail.com>

--- vipVideos_skipAd.user.js
+++ vipVideos_skipAd.user.js
@@ -10,7 +10,7 @@
 // @exclude        http://*.dj92cc.com/*
 //全面支持音悦台HTML5播放，详见 https://greasyfork.org/scripts/14593
 // @exclude        http://*.yinyuetai.com/*
-// @version        2016.7.15
+// @version        2016.7.19
 // @encoding       utf-8
 // @grant          unsafeWindow
 // ==/UserScript==
@@ -26,15 +26,18 @@ swfWhiteList = [
 	'http://v.6.cn/apple/player/',
 	'.plures.net/pts/swfbin/player/live.swf',//龙珠直播
 	'http://www.gaoxiaovod.com/ck/player.swf',
+	'http://assets.dwstatic.com/video/',//yy,虎牙
 ],
 PLAYER_URL = [
 	{
 		urls: [/^http:\/\/static\.youku\.com\/v.*?(?:play|load)er/],
 		run: function(p, v) {
-			unsafeWindow.scrollTo(0, 99);
-			unsafeWindow._ssPlayer = p.outerHTML.replace('direct','gpu');
-			unsafeWindow.document.querySelector("div#ab_pip").outerHTML =
-			'<a style="font-size:20px;" onclick="document.querySelector(\'#movie_player\').outerHTML=_ssPlayer, delete _ssPlayer, this.parentNode.removeChild(this);">换原播放器</a>';
+			if (doc.domain.endsWith('youku.com')) {
+				unsafeWindow.scrollTo(0, 99);
+				unsafeWindow._ssPlayer = p.outerHTML.replace('direct','gpu');
+				unsafeWindow.document.querySelector("div#ab_pip").outerHTML =
+				'<a style="font-size:20px;" onclick="document.querySelector(\'#movie_player\').outerHTML=_ssPlayer, delete _ssPlayer, this.parentNode.removeChild(this);">换原播放器</a>';
+			}
 			v = v.match(regYk);
 			v && setPlayer(p, youkuFormat(v[1]));
 		}
@@ -50,14 +53,13 @@ PLAYER_URL = [
 			v = swfAddr.match(this.urls[0]) || swfAddr.match(regYk) || v.match(regYk);
 			v && ykOutsitePlayer(v[1], p);
 		}
-	},{//iqiyi out
+	},/* {//iqiyi out
 		urls: [
-			/^https?:\/\/www\.iqiyi\.com\/(?:common\/flash)?player\/\d+\/(?:Share|Enjoy)?Player.*?\.swf/i,
-			//^http:\/\/dispatcher\.video\.qiyi\.com\/disp\/shareplayer\.swf/,
-			//^http:\/\/cdn\.aixifan\.com\/player\/cooperation\/AcFunXQiyi\.swf/,
+			/^http:\/\/dispatcher\.video\.qiyi\.com\/disp\/shareplayer\.swf/,
+			/^http:\/\/cdn\.aixifan\.com\/player\/cooperation\/AcFunXQiyi\.swf/,
 		],
 		run: qyOutsiteFormat
-	},{//iqiyi
+	}, */{//iqiyi
 		urls: [/^http:\/\/www\.iqiyi\.com\/common\/flashplayer/],
 		run: iqiyiFormat
 	},
@@ -72,26 +74,20 @@ function ykOutsitePlayer(vid, p) {
 function qyOutsiteFormat(p, v) {
 	let tvid = v.match(/\btvId=(\w+)/i)[1],
 	definitionID = v.match(/\b(?:definitionID|sourceId|vid)=(\w+)/)[1],
-	//http://dispatcher.video.qiyi.com/disp/shareplayer.swf
+	//${playerAddrs[0]}iqiyi_out.swf
 	s = `<embed width="100%" height="100%" allowscriptaccess="always" wmode="gpu" allowfullscreen="true" type="application/x-shockwave-flash" id="${p.id}" src="${playerAddrs[0]}iqiyi_out.swf" flashvars="vid=${definitionID}&tvid=${tvid}&autoPlay=1&showSearch=0&showSearchBox=0&autoHideControl=1&cid=qc_100001_300089&showDock=0">`;
 	setPlayer(p, s);
 }
 function iqiyiFormat(p, v) {
-	let s = v.replace(/&(?:cid|tipdataurl|\w+?Time|cpn\w|\w*?loader|adurl|exclusive)=[^&]*/g, '') + '&cid=qc_100001_300089';
+	let s = v.replace(/&(?:cid|\w+?Time|cpn\w|\w*?loader|adurl|exclusive)=[^&]*/g, '') + '&cid=qc_100001_300089';
 	s = `<embed play="true" allowfullscreen="true" wmode="gpu" type="application/x-shockwave-flash" width="100%" height="100%" id="flash" allowscriptaccess="always" src="${swfAddr}" flashvars="${s}">`;
 	setPlayer(p, s);
+	//setFlashParam(p, {'wmode': 'gpu','flashvars': s});
 }
 function openFlashGPU(p) {
-	isEmbed ? p.setAttribute('wmode', 'gpu') : setObjectVal(p, 'wmode', 'gpu');
+	isEmbed ? p.setAttribute('wmode', 'gpu') : setFlashParam(p, {'wmode': 'gpu'});
+	//p.parentNode.replaceChild(p.cloneNode(true), p);
 	refreshElem(p);
-}
-function isPlayer(p) {
-	if (swfWhiteList.some(x => swfAddr.includes(x))) return !0;
-	if (!p.width) return !1;
-	if (p.width.endsWith('%')) return !0;
-	if (parseInt(p.width) < 233 || parseInt(p.height) < 53) return !1;
-	return isEmbed ? p.matches('[allowFullScreen]') :
-		/"allowfullscreen"/i.test(p.innerHTML);
 }
 function refreshElem(o) {
 	let s = o.style.display;
@@ -102,25 +98,37 @@ function refreshElem(o) {
 			o.removeAttribute('style');
 	}, 9);
 }
+function isPlayer(p) {
+	if (swfWhiteList.some(x => swfAddr.includes(x))) return !0;
+	if (!p.width) return !1;
+	if (p.width.endsWith('%')) return !0;
+	if (parseInt(p.width) < 233 || parseInt(p.height) < 53) return !1;
+	return isEmbed ? p.matches('[allowFullScreen]') :
+		/"allowfullscreen"/i.test(p.innerHTML);
+}
 function setPlayer(play, oHtml) {
 	console.log('new player: ', oHtml);
 	play.outerHTML = oHtml;
 }
-function setObjectVal(p, name, v) {
-	let e = p.querySelector('embed');
-	e && e.setAttribute(name, v);
-	p.hasAttribute(name) && p.setAttribute(name, v);
-	name = name.toLowerCase();
-	for (let o of p.childNodes) {
-		if (o.name && o.name.toLowerCase() === name) {
-			o.value = v;
-			return;
+function setFlashParam(p, values) {
+	let i, o, e = p.querySelector('embed');
+	for (i in values) {
+		if (e) e.setAttribute(i, values[i]);
+		p.hasAttribute(i) && p.setAttribute(i, values[i]);
+	}
+	for (o of p.childNodes) {
+		i = o.name.toLowerCase();
+		if (i in values) {
+			o.value = values[i];
+			delete values[i];
 		}
 	}
-	e = doc.createElement('param');
-	e.name = name;
-	e.value = v;
-	p.appendChild(e);
+	for (i in values) {
+		e = doc.createElement('param');
+		e.name = i;
+		e.value = values[i];
+		p.appendChild(e);
+	}
 }
 function getFlashvars(p) {
 	let s = 'flashvars';
