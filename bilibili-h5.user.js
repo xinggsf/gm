ce922ce4f07bcf49d80d2e4d054c9bbd3b041458
xinggsf <xinggsf@gmail.com>
2017-06-16 16:42:25 +0800

    Signed-off-by: xinggsf <xinggsf@gmail.com>

--- bilibili-h5.user.js
+++ bilibili-h5.user.js
@@ -5,13 +5,13 @@
 // @description 启用B站的h5播放，自动宽屏、自动播放、原生右键菜单、关弹幕
 // @homepage    http://bbs.kafan.cn/thread-2061994-1-1.html
 // @downloadUrl https://raw.githubusercontent.com/xinggsf/gm/master/bilibili-h5.user.js
-// @version     2017.06.12
+// @version     2017.06.16
 // @include     *://www.bilibili.com/video/av*
 // @include     *://www.bilibili.com/*/html5player.html*
 // @include     *://bangumi.bilibili.com/anime/v/*
 // @include     *://bangumi.bilibili.com/anime/*/play*
 // @grant       unsafeWindow
-// @run-at      document-start
+// @run-at      document-body
 // ==/UserScript==
 'use strict';
 
@@ -22,48 +22,47 @@ const doClick = e => {
 };
 
 function setContextMenuHandler() {
-    const contextMenuEvent = unsafeWindow.$._data(q('.bilibili-player-video-wrap'), 'events').contextmenu[0];
-    const danmuMenuHandler = contextMenuEvent.handler;
+    const gMenuEvent = unsafeWindow.$._data(q('.bilibili-player-video-wrap'), 'events').contextmenu[0],
+    danmuMenuHandler = gMenuEvent.handler,
 
-    const isElementClicked = function (ele, x, y) {
+    isElementClicked = (ele, x, y) => {
         const r = ele.getBoundingClientRect();
         return x > r.left && x < r.right && y > r.top && y < r.bottom;
-    };
+    },
 
-    const danmuClicked = (arr, x, y) => [].some.call(arr, e => isElementClicked(e, x, y));
+    danmuClicked = (arr, x, y) => [].some.call(arr, e => isElementClicked(e, x, y)),
 
-    const newHandler = function (e) {
+    newHandler = e => {
         const a = document.querySelectorAll('.bilibili-danmaku');
         //if (e.target.matches('.bilibili-danmaku'))
         if (danmuClicked(a, e.clientX, e.clientY))
             danmuMenuHandler(e);
     };
 
-    contextMenuEvent.handler = exportFunction(newHandler, contextMenuEvent);
+    gMenuEvent.handler = exportFunction(newHandler, gMenuEvent);
 }
 
 localStorage.setItem('bilibililover', 'YESYESYES');
 localStorage.defaulth5 = 1;
 
-let num = 0;
-const timer = setInterval( ()=> {
-	if (num > 5) {
-        clearInterval(timer);
-		return;
-	}
-	++num;
-    let p = q('video');
-    if (p) {
-        p.setAttribute('autoplay','autoplay');//自动播放
+let mo = new MutationObserver(records => {
+    let v = q('video');
+	if (v) {
+        v.setAttribute('autoplay','autoplay');//自动播放
         q('.bilibili-player-video-btn-widescreen').click(); //开宽屏
         q('.bilibili-player-video-btn-repeat').click(); //循环播放
         doClick(q('i[name=ctlbar_danmuku_close]')); //关弹幕
         //doClick(q('ul.bpui-selectmenu-list li[data-value="3"]'));//超清
         setTimeout(setContextMenuHandler, 800);//原生右键菜单
-    }
-    p = p || q('#bofqi>iframe.player');//bangumi.bilibili.com的播放器在子窗口
-    if (p) {//顶层窗口和子窗口的定时器都得释放
-        clearInterval(timer);
+	}
+    //bangumi.bilibili.com的播放器在子窗口
+    if (v || q('#bofqi>iframe.player')) {
+		mo.disconnect();
+		mo = undefined;
         self === top && scrollTo(0, q('.player-wrapper').offsetTop);
     }
-}, 300);
+});
+mo.observe(document.body, {
+	childList : true,
+	subtree : true
+});
--- 视频站h5.user.js
+++ 视频站h5.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name             视频站h5
 // @description      视频站启用html5播放器
-// @version          0.2.5
+// @version          0.3.1
+// @homepage         http://bbs.kafan.cn/thread-2093014-1-1.html
 // @include          *://*.qq.com/*
 // @include          *://v.youku.com/v_show/id_*
 // @include          *://video.tudou.com/v/*
@@ -13,7 +14,7 @@
 // @exclude          https://www.panda.tv/
 // @grant            unsafeWindow
 // @run-at           document-start
-// @namespace https://greasyfork.org/users/7036-xinggsf
+// @namespace  https://greasyfork.org/users/7036-xinggsf
 // @updateURL  https://raw.githubusercontent.com/xinggsf/gm/master/视频站h5.user.js
 // ==/UserScript==
 'use strict';
@@ -24,12 +25,12 @@ Object.defineProperty(navigator, 'plugins', {
 	}
 });
 
-let siteFn, v, totalTime; //video element
+let siteFn, v, totalTime,
+	path = location.pathname;
 const stepLen = 5, //快进快退5秒
 skipLen = 21, //shift + 快进快退
 u = location.hostname,
 mDomain = u.split('.').reverse()[1],//主域名
-path = location.pathname,
 
 $ = id => document.getElementById(id),
 q = css => document.querySelector(css),
@@ -48,11 +49,12 @@ fakeUA = ua => Object.defineProperty(navigator, 'userAgent', {
 	enumerable: true
 }),
 getAllDuration = css => {
-	const a = q(css).innerHTML.split(':').reverse();
-	let n = 0, multiplier = 1;
-	for (let k of a) {
-		n += k * multiplier;
+	const a = q(css).innerHTML.split(':');
+	console.log(q(css), a);
+	let n = a.pop() *1, multiplier = 1;
+	for (let k of a.reverse()) {
 		multiplier *= 60;
+		n += k * multiplier;
 	}
 	return n || 2e4;
 },
@@ -60,6 +62,7 @@ hotKey = function (e) {
 	//console.log('hotKey', v.seeking, v.seekable);
 	// 可播放
 	//if (!v.seekable) return;
+	//if (v.readyState !== 4) return;
 	let seekPoint = ~~v.currentTime;
 	switch (e.keyCode) {
 	case 32: //space
@@ -74,28 +77,27 @@ hotKey = function (e) {
 		v.currentTime = seekPoint;
 		break;
 	case 39: //right
-		//if (v.readyState !== 4) return;
 		seekPoint += e.shiftKey ? skipLen : stepLen;
 		v.currentTime = seekPoint;
-		break;
 	}
 },
 onCanplay = function (e) {
 	//v.removeEventListener('oncanplay', onCanplay);
 	//v.oncanplay = null;//注释应对列表播放而不刷新页面
-	console.log('脚本启用html5播放器，事件： oncanplay');
-	if (totalTime && path === location.pathname) return;//分段视频返回
+	console.log('脚本[启用html5播放器]，事件oncanplay');
+	const r = path !== location.pathname;//点播了另一个视频
+	if (totalTime && !r) return;//分段视频返回
 	if (!totalTime) document.addEventListener('keydown', hotKey, !1);
+	totalTime = 0;
 	siteFn && siteFn();
-	//新页面、不刷新页面列表播放
-	if (!totalTime || path !== location.pathname)
-		totalTime = Math.round(v.duration);
+	totalTime = totalTime || Math.round(v.duration);
 	//跳过片头
 	if (totalTime > 666 && !['youku', 'le', 'lesports'].includes(mDomain))
 		v.currentTime = 66;
+	if (r) path = location.pathname;
 },
 init = () => {
-	let mo = new MutationObserver(mr => {
+	let mo = new MutationObserver(records => {
 		v = q('video');
 		if (v) {
 			//console.log('mo', v.oncanplay);
@@ -142,7 +144,8 @@ case 'tudou':
 	//fakeUA('Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) Chrome/55.0.10 Mobile');
 	siteFn = () => {
 		//获取播放时长
-		totalTime = getAllDuration('span.td-h5__player__console__time-total');
+		//totalTime = getAllDuration('span.td-h5__player__console__time-total');
+		totalTime = ~~q('meta[name=duration]').getAttribute('content');
 		const cur = ~~v.duration +1;
 		//console.log(cur, totalTime);
 		if (cur < totalTime) {
@@ -153,7 +156,8 @@ case 'tudou':
 			document.body.innerHTML = `<video width="100%" height="100%" autoplay controls src="${v.src}"/>`;
 			setTimeout(() => {
 				v = q('video');
-				v.oncanplay = onCanplay;
+				if (totalTime > 666) v.currentTime = 66;
+				//v.oncanplay = onCanplay;
 			}, 9);
 		}
 	};
