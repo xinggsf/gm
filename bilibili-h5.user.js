d7041bcce411155d4bd3bf72658a23fa8ecb91e1
xinggsf <xinggsf@gmail.com>
2017-07-24 17:57:44 +0800

    bilibili-h5解决选集后不能设置播放器

--- bilibili-h5.user.js
+++ bilibili-h5.user.js
@@ -5,7 +5,7 @@
 // @description 启用B站的h5播放，自动宽屏、自动播放、原生右键菜单、关弹幕
 // @homepage    http://bbs.kafan.cn/thread-2061994-1-1.html
 // @downloadUrl https://raw.githubusercontent.com/xinggsf/gm/master/bilibili-h5.user.js
-// @version     2017.06.16
+// @version     2017.07.24
 // @include     *://www.bilibili.com/video/av*
 // @include     *://www.bilibili.com/*/html5player.html*
 // @include     *://bangumi.bilibili.com/anime/v/*
@@ -21,22 +21,30 @@ const doClick = e => {
     e.click ? e.click(): e.dispatchEvent(new MouseEvent('click'));
 };
 
+const setPlayer = v => {
+	v.setAttribute('autoplay','');//自动播放
+	q('.bilibili-player-video-btn-widescreen').click(); //开宽屏
+	q('.bilibili-player-video-btn-repeat').click(); //循环播放
+	doClick(q('i[name=ctlbar_danmuku_close]')); //关弹幕
+	//doClick(q('ul.bpui-selectmenu-list li[data-value="3"]'));//超清
+	//setTimeout(setContextMenuHandler, 800);//原生右键菜单
+};
+
 function setContextMenuHandler() {
     const gMenuEvent = unsafeWindow.$._data(q('.bilibili-player-video-wrap'), 'events').contextmenu[0],
     danmuMenuHandler = gMenuEvent.handler,
 
-    isElementClicked = (ele, x, y) => {
-        const r = ele.getBoundingClientRect();
-        return x > r.left && x < r.right && y > r.top && y < r.bottom;
+    inElement = (e, x, y) => {
+		console.log(e.getBoundingClientRect);
+        const r = e.getBoundingClientRect();
+        return r.left < x < r.right && r.top < y < r.bottom;
     },
 
-    danmuClicked = (arr, x, y) => [].some.call(arr, e => isElementClicked(e, x, y)),
+    danmuClicked = (arr, x, y) => arr.some(e => inElement(e, x, y)),
 
     newHandler = e => {
-        const a = document.querySelectorAll('.bilibili-danmaku');
-        //if (e.target.matches('.bilibili-danmaku'))
-        if (danmuClicked(a, e.clientX, e.clientY))
-            danmuMenuHandler(e);
+        const a = [...document.querySelectorAll('.bilibili-danmaku')];
+        danmuClicked(a, e.clientX, e.clientY) && danmuMenuHandler(e);
     };
 
     gMenuEvent.handler = exportFunction(newHandler, gMenuEvent);
@@ -46,23 +54,32 @@ localStorage.setItem('bilibililover', 'YESYESYES');
 localStorage.defaulth5 = 1;
 
 let mo = new MutationObserver(records => {
-    let v = q('video');
-	if (v) {
-        v.setAttribute('autoplay','autoplay');//自动播放
-        q('.bilibili-player-video-btn-widescreen').click(); //开宽屏
-        q('.bilibili-player-video-btn-repeat').click(); //循环播放
-        doClick(q('i[name=ctlbar_danmuku_close]')); //关弹幕
-        //doClick(q('ul.bpui-selectmenu-list li[data-value="3"]'));//超清
-        setTimeout(setContextMenuHandler, 800);//原生右键菜单
-	}
     //bangumi.bilibili.com的播放器在子窗口
-    if (v || q('#bofqi>iframe.player')) {
+    if (location.host.startsWith('bangumi.') && q('.player-wrapper')) {
 		mo.disconnect();
 		mo = undefined;
-        self === top && scrollTo(0, q('.player-wrapper').offsetTop);
+        scrollTo(0, q('.player-wrapper').offsetTop);
+		return;
     }
+	for (let r of records) {
+		if (r.type === 'attribute' && r.target.matches('video')) {
+			setPlayer(r.target);
+			return;
+		}
+
+		if (r.type === 'childList' && r.addedNodes)
+			for (let v of r.addedNodes) {
+				if (v.nodeName ==='VIDEO') {
+					setPlayer(v);
+					self === top && scrollTo(0, q('.player-wrapper').offsetTop);
+					return;
+				}
+			}
+	}
 });
 mo.observe(document.body, {
 	childList : true,
-	subtree : true
+	subtree : true,
+	attributes: true,
+	attributeFilter: ['src']
 });
