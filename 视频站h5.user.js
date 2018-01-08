71dbcc93ffadb5b336908096eefa2004e017fb17
xinggsf <xinggsf@gmail.com>
2018-01-08 13:06:03 +0800

    Signed-off-by: xinggsf <xinggsf@gmail.com>

--- 视频站h5.user.js
+++ 视频站h5.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name             视频站启用html5播放器
 // @description      拥抱html5，告别Flash。添加快捷键：快进、快退、暂停/播放、音量、下一集、切换[万能网页]全屏、上下帧、播放速度。支持站点：优.土、QQ、新浪、微博、网易视频[娱乐、云课堂、新闻]、搜狐、乐视、央视、风行、百度云视频、熊猫、龙珠、战旗直播等，可自定义站点
-// @version          0.67
+// @version          0.68
 // @homepage         http://bbs.kafan.cn/thread-2093014-1-1.html
 // @include          *://pan.baidu.com/*
 // @include          *://v.qq.com/*
@@ -40,9 +40,29 @@
 // @updateURL  https://raw.githubusercontent.com/xinggsf/gm/master/视频站h5.user.js
 // ==/UserScript==
 'use strict';
-
+if (top !== self) return;
 if (window.chrome)
 	NodeList.prototype[Symbol.iterator] = HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+
+const q = css => document.querySelector(css),
+doClick = e => {
+	if (e) e.click ? e.click() : e.dispatchEvent(new MouseEvent('click'));
+},
+r1 = (regp, s) => regp.test(s) && RegExp.$1,
+fakeUA = ua => Object.defineProperty(navigator, 'userAgent', {
+	value: ua,
+	writable: false,
+	configurable: false,
+	enumerable: true
+}),
+getMainDomain = host => {
+	let a = host.split('.'),
+	i = a.length -2;
+	if (['com','net','org','gov','edu'].includes(a[i])) i--;
+	return a[i];
+},
+ua_samsung = 'Mozilla/5.0 (Linux; U; Android 4.0.4; GT-I9300 Build/IMM76D) AppleWebKit/534.30 Version/4.0 Mobile Safari/534.30',
+ua_ipad2 = 'Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3';
 
 class Fullscreen {
 	constructor(video) {
@@ -119,7 +139,7 @@ class WebFullscreen {
 		do {
 			e = p;
 			p = e.parentNode;
-		} while (p !== d && (p.clientWidth - w < 5) && (p.clientHeight - h < 5))
+		} while (p !== d && p.clientWidth - w < 5 && p.clientHeight - h < 5);
 		//console.dirxml(e);
 		return e;
 	}
@@ -159,164 +179,175 @@ class WebFullscreen {
 	}
 }
 
-let v, vList, totalTime, initCb, webFull, fullScreen,
-	isLive = !1,
-	oldCanplay = null,
-	playerInfo = {},
-	path = location.pathname;
+let webFull, fullScreen;
 
-const u = location.hostname,
-mDomain = u.includes('.sina.') ? 'sina' : u.split('.').reverse()[1],//主域名
-ua_samsung = 'Mozilla/5.0 (Linux; U; Android 4.0.4; GT-I9300 Build/IMM76D) AppleWebKit/534.30 Version/4.0 Mobile Safari/534.30',
-ua_ipad2 = 'Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
-q = css => document.querySelector(css),
-r1 = (regp, s) => regp.test(s) && RegExp.$1,
-fakeUA = ua => Object.defineProperty(navigator, 'userAgent', {
-	value: ua,
-	writable: false,
-	configurable: false,
-	enumerable: true
-}),
-getAllDuration = css => {
-	const a = q(css).innerHTML.split(':');
-	let n = a.pop() | 0, multiplier = 1;
-	for (let k of a.reverse()) {
-		multiplier *= 60;
-		n += k * multiplier;
+const { host, pathname: path } = location,
+u = getMainDomain(host),//主域名
+//容器，登记事件处理方法中的回调
+events = {
+	on(name, fn) {
+		this[name] = fn;
 	}
-	return n || 2e4;
 },
-doClick = css => {
-	if (!css) return !1;
-	const x = q(css);
-	if (x) x.click ? x.click() : x.dispatchEvent(new MouseEvent('click'));
-	return !!x;
-},
-removeAllElem = css => {
-	for (let e of document.querySelectorAll(css)) e.remove();
-},
-getVideo = () => {
-	vList = vList || document.getElementsByTagName('video');
-	if (v.offsetWidth>1) return;
-	for (let e of vList)
-		if (e.offsetWidth>1) {
-			e.playbackRate = v.playbackRate;
-			e.volume = v.volume;
-			v = e;
-			break;
-		}
-},
-onCanplay = function(e) {
-	v.oncanplay = null;
-	console.log('脚本[启用html5播放器]，事件oncanplay');
-	if (playerInfo.onMetadata) {
-		playerInfo.onMetadata();
-		delete playerInfo.onMetadata;
-	}
-	oldCanplay && oldCanplay(e);
-	totalTime = totalTime || Math.round(v.duration);
-	if (!isLive && totalTime > 666 && 'qq' !== mDomain) setTimeout(() => {
-		v.currentTime = 66;//跳过片头
-	}, 9);
-},
-hotKey = function(e) {
-	//判断ctrl,alt,shift三键状态，防止浏览器快捷键被占用
-	if (e.ctrlKey || e.altKey || /INPUT|TEXTAREA/.test(e.target.nodeName))
-		return;
-	if (e.shiftKey && ![13,37,39].includes(e.keyCode))
-		return;
-	if (isLive && [37,39,78,88,67,90].includes(e.keyCode))
-		return;
-	vList && getVideo();
-	if (!v || v.offsetWidth === 0) v = q('video');
-	let n;
-	switch (e.keyCode) {
-	case 32: //space
-		if (playerInfo.disableSpace) return;
-		v.paused ? v.play() : v.pause();
-		e.preventDefault();
-		e.stopPropagation();
-		break;
-	case 37: //left
-		n = e.shiftKey ? -27 : -5; //快退5秒,shift加速
-	case 39: //right
-		n = n || (e.shiftKey ? 27 : 5); //快进5秒,shift加速
-		v.currentTime += n;
-		break;
-	case 78: // N 下一首
-		doClick(playerInfo.nextCSS);
-		break;
-	//case 80: // P 上一首
-	case 38: //加音量
-		n = .1;
-	case 40: //降音量
-		n = n || -0.1;
-		n += v.volume;
-		if (0 <= n && n <= 1) v.volume = n;
-		e.preventDefault();
-		break;
-	case 13: //全屏
-		if (e.shiftKey) {
-			webFull ? webFull.toggle() : doClick(playerInfo.webfullCSS);
+app = {
+	el: null,//video
+	isLive: !1,
+	_duration: 0,
+	get duration() {
+		if (this._duration) return this._duration;
+
+		if (this.timeCSS) {
+			const a = q(this.timeCSS).innerHTML.split(':');
+			let n = a.pop() | 0, multiplier = 1;
+			for (let k of a.reverse()) {
+				multiplier *= 60;
+				n += k * multiplier;
+			}
+			this._duration = n || 2e4;
 		} else {
-			fullScreen ? fullScreen.toggle() : doClick(playerInfo.fullCSS);
+			this._duration = Math.round(this.el.duration);
 		}
-		break;
-	case 27: //esc
-		if (Fullscreen.isFull()) {
-			fullScreen ? fullScreen.exit() : doClick(playerInfo.fullCSS);
-		}
-		else if (WebFullscreen.isFull(v)) {
-			webFull ? webFull.toggle() : doClick(playerInfo.webfullCSS);
-		}
-		break;
-	case 67: //按键C：加速播放 +0.1
-		n = .1;
-	case 88: //按键X：减速播放 -0.1
-		n = n || -0.1;
-		n += v.playbackRate;
-		if (0 < n && n <= 16) v.playbackRate = n;
-		break;
-	case 90: //按键Z：正常速度播放
-		v.playbackRate = 1;
-		break;
-	case 70: //按键F：下一帧
-		n = .03;
-	case 68: //按键D：上一帧
-		n = n || -0.03;
-		if (!v.paused) v.pause();
-		v.currentTime += n;
-	}
-},
-doHls = () => {
-	if (!v || !v.src.includes('.m3u8') || !Hls.isSupported()) return;
-	const hls = new Hls();
-	hls.loadSource(v.src);
-	hls.attachMedia(v);
-	hls.on(Hls.Events.MANIFEST_PARSED, () => v.play());
-},
-init = cb => {
-	new MutationObserver(function(records) {
-		v = q('video');
+		return this._duration;
+	},
+
+	vList: null,
+	getVideo() {
+		this.vList = this.vList || document.getElementsByTagName('video');
+		if (this.el.offsetWidth>1) return;
+		for (let e of this.vList)
+			if (e.offsetWidth>1) {
+				e.playbackRate = this.el.playbackRate;
+				e.volume = this.el.volume;
+				this.el = e;
+				break;
+			}
+	},
+	_convertView(btn) {
+		// !btn.clientWidth ?
+		const e = btn.style.display === 'none' ? btn.nextElementSibling : btn;
+		doClick(e);
+	},
+	observe() {
+		const v = q('video');
 		if (v) {
-			oldCanplay = v.oncanplay;
-			v.oncanplay = onCanplay;
-			this.disconnect();
-			document.addEventListener('keydown', hotKey, !1);
+			this._mo.disconnect();
+			this.el = v;
+			this.oldCanplay = v.oncanplay;
+			v.oncanplay = this.onCanplay.bind(this);
+			document.addEventListener('keydown', this.hotKey.bind(this), !1);
 			setTimeout(() => {
 				v.focus();
-				if (!playerInfo.webfullCSS) webFull = new WebFullscreen(v);
+				if (!this.webfullCSS) webFull = new WebFullscreen(v);
 			}, 300);
-			if (!playerInfo.fullCSS) fullScreen = new Fullscreen(v);
-			if (playerInfo.timeCSS) totalTime = getAllDuration(playerInfo.timeCSS);
-			cb && cb();
+			if (!this.fullCSS)
+				fullScreen = new Fullscreen(v);
+			else {
+				this.btnFS = q(this.fullCSS);
+				//进入、退出、切换全屏，三合一
+				this.fullScreen = () => this._convertView(this.btnFS);
+			}
+			if (this.webfullCSS) {
+				this.btnWFS = q(this.webfullCSS);
+				this.webFullScreen = () => this._convertView(this.btnWFS);
+			}
+			if (this.nextCSS) this.btnNext = q(this.nextCSS);
+			events.init && events.init();
 		}
-	}).observe(document.documentElement, {
-		childList : true, subtree : true
-	});
+	},
+	onCanplay(e) {
+		const v = this.el;
+		v.oncanplay = null;
+		console.log('脚本[启用html5播放器]，事件oncanplay');
+		events.canplay && events.canplay();
+		this.oldCanplay && this.oldCanplay(e);
+		if (!this.isLive && this.duration > 666 && !/qq|le/.test(u)) setTimeout(() => {
+			v.currentTime = 66;//跳过片头
+		}, 9);
+	},
+	hotKey(e) {
+		//判断ctrl,alt,shift三键状态，防止浏览器快捷键被占用
+		if (e.ctrlKey || e.altKey || /INPUT|TEXTAREA/.test(e.target.nodeName))
+			return;
+		if (e.shiftKey && ![13,37,39].includes(e.keyCode))
+			return;
+		if (this.isLive && [37,39,78,88,67,90].includes(e.keyCode))
+			return;
+		this.getVideo();
+		const v = this.el;
+		// if (!v || v.offsetWidth === 0) v = q('video');
+		let n;
+		switch (e.keyCode) {
+		case 32: //space
+			if (this.disableSpace) return;
+			v.paused ? v.play() : v.pause();
+			e.preventDefault();
+			e.stopPropagation();
+			break;
+		case 37: //left
+			n = e.shiftKey ? -27 : -5; //快退5秒,shift加速
+		case 39: //right
+			n = n || (e.shiftKey ? 27 : 5); //快进5秒,shift加速
+			v.currentTime += n;
+			break;
+		case 78: // N 下一首
+			doClick(this.btnNext);
+			break;
+		//case 80: // P 上一首
+		case 38: //加音量
+			n = .1;
+		case 40: //降音量
+			n = n || -0.1;
+			n += v.volume;
+			if (0 <= n && n <= 1) v.volume = n;
+			e.preventDefault();
+			break;
+		case 13: //全屏
+			if (e.shiftKey)
+				webFull ? webFull.toggle() : this.webFullScreen();
+			else
+				fullScreen ? fullScreen.toggle() : this.fullScreen();
+			break;
+		case 27: //esc
+			if (Fullscreen.isFull())
+				fullScreen ? fullScreen.exit() : this.fullScreen();
+			else if (WebFullscreen.isFull(v))
+				webFull ? webFull.toggle() : this.webFullScreen();
+			break;
+		case 67: //按键C：加速播放 +0.1
+			n = .1;
+		case 88: //按键X：减速播放 -0.1
+			n = n || -0.1;
+			n += v.playbackRate;
+			if (0 < n && n <= 16) v.playbackRate = n;
+			break;
+		case 90: //按键Z：正常速度播放
+			v.playbackRate = 1;
+			break;
+		case 70: //按键F：下一帧
+			n = .03;
+		case 68: //按键D：上一帧
+			n = n || -0.03;
+			if (!v.paused) v.pause();
+			v.currentTime += n;
+		}
+	},
+	doHls() {
+		const v = this.el;
+		if (!v || !v.src.includes('.m3u8') || !Hls.isSupported()) return;
+		const hls = new Hls();
+		hls.loadSource(v.src);
+		hls.attachMedia(v);
+		hls.on(Hls.Events.MANIFEST_PARSED, () => v.play());
+	},
+	init() {
+		this._mo = new MutationObserver(this.observe.bind(this));
+		this._mo.observe(document.documentElement, {
+			childList : true, subtree : true
+		});
+	}
 };
 
-if (mDomain !== 'zhanqi') Object.defineProperty(navigator, 'plugins', {
+if (u !== 'zhanqi') Object.defineProperty(navigator, 'plugins', {
 	get: function() {
 		return { length: 0 };
 	}
@@ -324,38 +355,35 @@ if (mDomain !== 'zhanqi') Object.defineProperty(navigator, 'plugins', {
 
 const router = {
 	qq() {
-		playerInfo = {
+		Object.assign(app, {
 			disableSpace: true,
-			nextCSS: 'txpdiv.txp_btn_next',
-			webfullCSS: 'txpdiv[data-report="browser-fullscreen"]',
-			fullCSS: 'txpdiv.txp_btn_fullscreen',
-			onMetadata: getVideo
-		};
+			nextCSS: '.txp_btn_next',
+			webfullCSS: '.txp_btn_fake',
+			fullCSS: '.txp_btn_fullscreen',
+		});
+		events.on('canplay', app.getVideo);
 		fakeUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:48.0) Gecko/20100101 Firefox/48.0');
 	},
 
 	youku() {
-		sessionStorage.P_l_h5 = 1;
-		playerInfo = {
-			fullCSS: '.control-icon.control-settings-icon ~ button:not([style="display: none;"])',
-			timeCSS: 'span.control-time-duration',
-			onMetadata: () => {
-				q('.youku-layer-logo').remove();
-				if (v.src.startsWith('http')) getVideo();//初始化vList，旧版用flv.js~地址以blob:开头
-				//修正下一个按钮无效
-				const btn = q('button.control-next-video');
-				if (btn && btn.offsetWidth>1) {
-					let e = q('.program.current');
-					e = e && e.closest('.item') || q('.item.current');
-					e = e.nextSibling;
-					if (!e) return;
-					e = e.querySelector('a');//下一个视频链接
-					btn.addEventListener('click', ev => e.click());
-					const attr = e.closest('[item-id]').getAttribute('item-id');
-					playerInfo.nextCSS = `[item-id="${attr}"] a`;
-				}
+		events.on('canplay', () => {
+			q('.youku-layer-logo').remove();
+			if (app.el.src.startsWith('http')) app.getVideo();//初始化vList，旧版用flv.js~地址以blob:开头
+			//修正下一个按钮无效
+			const btn = q('button.control-next-video');
+			if (btn && btn.offsetWidth>1) {
+				let e = q('.program.current');
+				e = e && e.closest('.item') || q('.item.current');
+				e = e.nextSibling;
+				if (!e) return;
+				e = e.querySelector('a');//下一个视频链接
+				btn.addEventListener('click', ev => e.click());
+				app.btnNext = e;
 			}
-		};
+		});
+		app.fullCSS = '.control-fullscreen-icon';
+		app.timeCSS = '.control-time-duration';
+		sessionStorage.P_l_h5 = 1;
 	},
 
 	cctv() {
@@ -372,31 +400,29 @@ const router = {
 		if (isFX57 && isFX57 < 57) return true;
 		fakeUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 Version/7.0.3 Safari/7046A194A');
 		//totalTime = __INFO__.video.duration;
-		playerInfo = {
+		Object.assign(app, {
 			timeCSS: 'span.hv_total_time',
 			nextCSS: 'div.hv_ico_next',
 			webfullCSS: 'span.hv_ico_webfullscreen',
 			fullCSS: 'span.hv_ico_screen'
-		};
+		});
 	},
 
 	sohu() {
 		fakeUA(ua_samsung);
-		playerInfo = {
+		Object.assign(app, {
 			timeCSS: 'span.x-duration-txt',
 			nextCSS: 'li.on[data-vid]+li > a',
 			fullCSS: 'div.x-fs-btn'
-		};
+		});
 	},
 
 	fun() {
-		if (u.startsWith('m.')) {
+		if (host.startsWith('m.')) {
 			if (!path.includes('play')) return true;//非播放页，不执行init()
 			/^\/[mv]/.test(path) && location.assign(path.replace('/', '/i') + location.search);
-			playerInfo = {
-				nextCSS: 'a.btn.next-btn',
-				fullCSS: 'a.btn.full-btn'
-			};
+			app.nextCSS = 'a.btn.next-btn';
+			app.fullCSS = 'a.btn.full-btn';
 			GM_addStyle('div.p-ip-wrap{overflow-y: auto !important;}');
 			return;
 		}
@@ -407,7 +433,6 @@ const router = {
 			mid && location.assign(`//m.fun.tv/implay/?mid=${mid}&vid=${vid}`);
 			location.assign('//m.fun.tv/ivplay/?vid='+vid);
 		}
-
 		mid && setTimeout(() => {
 			vid = unsafeWindow.vplay.videoid;
 			vid && location.assign(`//m.fun.tv/implay/?mid=${mid}&vid=${vid}`);
@@ -417,24 +442,23 @@ const router = {
 
 	panda() {
 		localStorage.setItem('panda.tv/user/player', '{"useH5player": true}');
-		playerInfo = {
-			webfullCSS: 'span.h5player-control-bar-fullscreen',
-			fullCSS: 'span.h5player-control-bar-allfullscreen'
-		};
-		isLive = true;
+		app.webfullCSS = 'span.h5player-control-bar-fullscreen';
+		app.fullCSS = 'span.h5player-control-bar-allfullscreen';
+		app.isLive = true;
 	},
 
 	longzhu() {
-		isLive = true;
+		app.isLive = true;
 		fakeUA(ua_ipad2);
-		initCb = () => {
+		events.on('init', () => {
+			const v = app.el;
 			if (v.src && v.src.includes('.m3u8'))
-				doHls();
+				app.doHls();
 			else {
 				q('#landscape_dialog').remove();
 				new MutationObserver(function(records) {
 					this.disconnect();
-					doHls();
+					app.doHls();
 				})
 				.observe(v, {
 					attributes: true,
@@ -442,7 +466,7 @@ const router = {
 				});
 				setTimeout(() => q('.player.report-rbi-click').click(), 1200);
 			}
-		};
+		});
 	},
 
 	zhanqi() {
@@ -455,18 +479,18 @@ const router = {
 			let s = e.children.flashvars.value,
 			url = r1(/PlayUrl=([^&]+)/, s);//视频
 			if (!url) {
-				isLive = true;
+				app.isLive = true;
 				s = r1(/VideoLevels=([^&]+)/, s);//直播
 				s = atob(s);
 				url = JSON.parse(s).streamUrl;
 			}
 			e.parentNode.innerHTML = `<video width="100%" height="100%" autoplay controls src="${url}"/>`;
 		}, 300);
-		initCb = doHls;
+		events.on('init', app.doHls);
 	}
 };
 router.cntv = router.cctv;
 router.lesports = router.le;
 router['163'] = router.sina;
 
-if (!router[mDomain] || !router[mDomain].call(null)) init(initCb);
+if (!router[u] || !router[u].call(null)) app.init();
