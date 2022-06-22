791d82e4e473f0493503650336241a37a74df7fb
xinggsf <xinggsf@gmail.com>
2022-06-23 06:20:03 +0800

    更新 pcbeta.user.js

--- pcbeta.user.js
+++ pcbeta.user.js
@@ -1,18 +1,11 @@
 // ==UserScript==
 // @name         去pcbeta.com的防广告提示
 // @namespace    pcbeta.xinggsf
-// @version      1.2
+// @version      1.3
 // @description  去pcbeta.com的防广告提示
 // @author       xinggsf
 // @match        https://*.pcbeta.com/*
-// @updateURL    https://gitee.com/xinggsf/gm/raw/master/pcbeta.user.js
-// @run-at       document-start
 // ==/UserScript==
 
 'use strict';
-const p = Node.prototype;
-const ib = p.insertBefore;
-p.insertBefore = function (...a) {
-	if (this.id === 'wp') throw '去广告';
-	return ib.apply(this, a);
-};
+document.querySelector('.adsbygoogle').closest('#wp>div').remove();
