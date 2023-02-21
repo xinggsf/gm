c3c747d7c1cbc9040ed4f85e790df06728a93513
xinggsf <xinggsf@gmail.com>
2023-02-21 08:58:30 +0800

    Update 视频站h5.user.js

--- 视频站h5.user.js
+++ 视频站h5.user.js
@@ -294,6 +294,7 @@ class FullPage {
 				width: 100% !important;
 				height: 100% !important;
 				padding: 0 !important;
+				margin: 0 !important;
 				top: 0 !important;
 				left: 0 !important;
 				background: #000 !important;
