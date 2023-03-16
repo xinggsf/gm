bfb7da6318fd22f117a08e665496ffece38ba360
xinggsf <xinggsf@gmail.com>
2023-03-16 09:24:35 +0800

    Create set-dandanzan-buff.user.js

---
+++ set-dandanzan-buff.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        ray-dandanzan.cf
+// @description 设置缓存区大小
+// @namespace   set-dandanzan-buffSize
+// @match       https://dandanzan.cf/movie/*
+// @match       https://nunuyingyuan.com/movie/*
+// @grant       unsafeWindow
+// @run-at      document-end
+// @version     1.0
+// @author      ray
+// ==/UserScript==
+
+const buffSize = 180; // 缓存区大小，单位秒
+const sleep = ms => new Promise(resolve => { setTimeout(resolve, ms) });
+(async function exec() {
+    if ( !unsafeWindow.player ) {
+		await sleep(100);
+		exec();
+	}
+	else unsafeWindow.player.configure({
+		streaming: {
+			bufferingGoal: buffSize + 10,
+			// rebufferingGoal: 15,
+			bufferBehind: buffSize,
+		}
+	});
+})();
